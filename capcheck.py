cf1ed3a9ce36ad084728dbf7debb42c984abb106
Brian Kidney <bkidney@briankidney.ca>
2019-06-11 23:00:03 -0230

    Adds support for multiple libraries.
    - Gets required libraries from ldd
    - Loads and merges multiple call graphs

--- capcheck.py
+++ capcheck.py
@@ -13,27 +13,44 @@ elfdata = elfdata.split('\n')
 fns = []
 for line in elfdata:
     if len(line.split()) > 8:
-        fn, lib = line.split()[7].split('@')
+        type = line.split()[6]
 
-        if (lib.startswith("FBSD")):
+        if type == "UND":
+            fn, lib = line.split()[7].split('@')
             fns.append(fn)
 
 fns = list(dict.fromkeys(fns))
+print(fns)
 
-gtmp = pgv.AGraph(sys.argv[2])
-graph = nx.DiGraph(gtmp)
+ldd_data = subprocess.check_output(['ldd', binary])
+ldd_data = ldd_data.split('\n')
 
+libs= []
+for line in ldd_data:
+    if len(line.split()) > 1:
+        lib = line.split()[0]
+        libs.append(lib)
+
+
+callgraph = nx.DiGraph()
 fn_to_node = dict()
 node_to_fn = dict()
-for i in gtmp.nodes():
-    fn_to_node[ i.attr['label'][1:-1]] =  i.name
-    node_to_fn[ i.name ] = i.attr['label'][1:-1]
+for lib in libs:
+    gtmp = pgv.AGraph(sys.argv[2] + "/" + lib + ".bc.dot")
+    for i in gtmp.nodes():
+        fn_to_node[ i.attr['label'][1:-1]] =  i.name
+        node_to_fn[ i.name ] = i.attr['label'][1:-1]
+    graph = nx.DiGraph(gtmp)
+    tmp = callgraph.copy()
+    callgraph = nx.compose(tmp, graph)
+
+# nx.nx_agraph.write_dot(callgraph, "test.dot")
 
 fn_to_capunsafe = dict()
 with open(sys.argv[3]) as f:
     for line in f:
         if line.strip() in fn_to_node:
-            callers = nx.algorithms.dag.ancestors(graph, fn_to_node[line.strip()])
+            callers = nx.algorithms.dag.ancestors(callgraph, fn_to_node[line.strip()])
 
             for call in callers:
                 fn = node_to_fn[call]
